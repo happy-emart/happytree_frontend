b5ea147909ebacfaf43e529c3e2f2b1277311e84
su-giana <giananews@gmail.com>
2023-07-10 20:36:17 +0900

    10 Jul. (Sujee) : received received letters implementation done

--- lib/first_tab.dart
+++ lib/first_tab.dart
@@ -1,11 +1,9 @@
+import 'dart:core';import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/writing_letter.dart';
 import 'package:http/http.dart' as http;
-import 'package:mysql_client/mysql_client.dart';
-import 'dart:io';
-import 'package:mysql1/mysql1.dart';
-import 'crud_service.dart' as crud;
-import 'post.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'dart:math' as math;
@@ -47,16 +45,6 @@ import 'dart:math' as math;
 // }
 
 
-void main() => runApp(MaterialApp(
-    builder: (context, child) {
-      return Directionality(textDirection: TextDirection.ltr, child: child!);
-    },
-    title: 'GNav',
-    theme: ThemeData(
-      primaryColor: Colors.grey[800],
-    ),
-    home: FirstPage()));
-
 class FirstPage extends StatefulWidget {
   @override
   _FirstState createState() => _FirstState();
@@ -70,7 +58,47 @@ class _FirstState extends State<FirstPage> {
   late double poleHeight = deviceWidth*0.13;
   late final startPoint = (centerHeight - deviceWidth)*0.5;
   late final endPoint = startPoint + deviceWidth - poleHeight;
-  Stack buildTree(double cntrHeight, double cntrWidth, BuildContext context) {
+  List<int> fruits = [];
+
+  @override
+  void initState()
+  {
+    super.initState();
+    fetchFruits();
+  }
+
+  Future<String?> getJwtToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString('jwtToken');
+  }
+
+  void fetchFruits() async {
+    final String Url = "http://127.0.0.1:8080/received_letters";
+    final jwtToken = await getJwtToken();
+    print('Bearer $jwtToken');
+    final request = Uri.parse(Url);
+    final headers = <String, String> {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $jwtToken'
+    };
+    try
+    {
+      final response = await http.get(request, headers: headers);
+      print(response.body);
+    }
+    catch(error)
+    {
+      print('error : $error');
+    }
+
+    fruits.add(1);
+    fruits.add(2);
+    fruits.add(3);
+    fruits.add(4);
+  }
+
+  Stack buildTree(double cntrHeight, double cntrWidth, BuildContext context, List<dynamic> fruits) {
     return Stack(
             children: [
               Column(
@@ -93,10 +121,8 @@ class _FirstState extends State<FirstPage> {
                   ),
                 ],
               ),
-              createFruit(context , startPoint + math.Random().nextDouble()*(endPoint-startPoint), 48+ math.Random().nextDouble()*(deviceWidth-48), 1),
-              createFruit(context , startPoint + math.Random().nextDouble()*(endPoint-startPoint), 48+math.Random().nextDouble()*(deviceWidth-48), 2),
-              createFruit(context , startPoint + math.Random().nextDouble()*(endPoint-startPoint), 48+math.Random().nextDouble()*(deviceWidth-48), 3),
-              createFruit(context , startPoint + math.Random().nextDouble()*(endPoint-startPoint), 48+math.Random().nextDouble()*(deviceWidth-48), 4),
+              for(var fruit in fruits)
+                createFruit(context , startPoint + math.Random().nextDouble()*(endPoint-startPoint), 48+ math.Random().nextDouble()*(deviceWidth-48), fruit),
               ],
               // buildImageStack(5),
           );
@@ -182,11 +208,12 @@ class _FirstState extends State<FirstPage> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
+
   late List<Widget> _widgetOptions = <Widget>[
-    buildTree(centerWidth, centerWidth, context),
     Stack(
       children:[
-      buildTree(centerWidth, centerWidth, context),
+      buildTree(centerWidth, centerWidth, context, fruits),
       Positioned(
         left: 0,
         right: 0,
@@ -316,300 +343,3 @@ void startFirstPage(BuildContext context) {
     MaterialPageRoute(builder: (context) => FirstPage()),
   );
 }
-
-// class FirstTab extends StatelessWidget {
-//   const FirstTab({super.key});
-//   // List<Widget> buildImageStack(int numOfPost) {
-//   //   return List.generate(numOfPost, (_) {
-//   //     return Image.asset('assets/images/apple.png');
-//   //   });
-//   // }
-//   // List<Widget> buildImageStackDemo(int numOfPost) {
-//   //   return List.generate(numOfPost, (_) {
-//   //     return Image.asset('assets/images/apple.png');
-//   //   });
-//   // }
-//   // var last = [()]  
-//   int _selectedIndex = 0;
-//   @override
-//   Widget build(BuildContext context) {
-//     double deviceWidth = MediaQuery.of(context).size.width;  // 화면의 가로 크기
-//     double deviceHeight = MediaQuery.of(context).size.height; // 화면의 세로 크기
-
-//     // double cntrHeight = deviceHeight/2;
-//     double cntrWidth = deviceWidth/1;
-//     return MaterialApp(
-//       /**
-//        * Tab 사용법
-//        * 스크린을 TabController 위젯으로 구성함
-//        * TabBar: 사용할 탭을 구성. appBar의 bottom으로 구현
-//        * TabBarView: 탭이 선택될 시 디스플레이할 컨텐트 구성. body로 구현
-//        */
-//       home: DefaultTabController(
-//         // 탭의 수 설정
-//         length: 3,
-//         child: Scaffold(
-//           bottomNavigationBar: Container(
-//             decoration: BoxDecoration(
-//               color: Colors.white,
-//               boxShadow: [
-//                 BoxShadow(
-//                   blurRadius: 20,
-//                   color: Colors.black.withOpacity(.1),
-//                 )
-//               ],
-//             ),
-//             child: SafeArea(
-//               child: Padding(
-//                 padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-//                 child: GNav(
-//                   rippleColor: Colors.grey[300]!,
-//                   hoverColor: Colors.grey[100]!,
-//                   gap: 8,
-//                   activeColor: Colors.black,
-//                   iconSize: 24,
-//                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-//                   duration: Duration(milliseconds: 400),
-//                   tabBackgroundColor: Colors.grey[100]!,
-//                   color: Colors.black,
-//                   tabs: [
-//                     GButton(
-//                       icon: LineIcons.home,
-//                       text: 'Home',
-//                     ),
-//                     GButton(
-//                       icon: LineIcons.heart,
-//                       text: 'Likes',
-//                     ),
-//                     GButton(
-//                       icon: LineIcons.search,
-//                       text: 'Search',
-//                     ),
-//                     GButton(
-//                       icon: LineIcons.user,
-//                       text: 'Profile',
-//                     ),
-//                   ],
-//                   selectedIndex: _selectedIndex,
-//                   onTabChange: (index) {
-//                     setState(() {
-//                       _selectedIndex = index;
-//                     });
-//                   },
-//                 ),
-//               ),
-//             ),
-//           ),
-//           // appBar: AppBar(
-//           //   title: Row(
-//           //     children: [
-//           //       Text("fdsaf"), // Title widget
-//           //       SizedBox(width: 16.0), // Optional spacing between title and TabBar
-//           //     ],
-//           //   ),
-//           //   // TabBar 구현. 각 컨텐트를 호출할 탭들을 등록
-//           //   flexibleSpace: TabBar(
-//           //     tabs: [
-//           //       Tab(text: "나의 나무",),
-//           //       Tab(text: "너의 나무",),
-//           //       Tab(icon: Icon(Icons.directions_bike)),
-//           //     ],
-//           //   ),
-//           // ),
-//           // TabVarView 구현. 각 탭에 해당하는 컨텐트 구성
-//           body: TabBarView(
-//             children: [
-//               buildTree(cntrWidth, cntrWidth, context),
-              // Stack(
-              //   children:[
-              //   buildTree(cntrWidth, cntrWidth, context),
-              //   Positioned(
-              //     left: 0,
-              //     right: 0,
-              //     bottom: deviceHeight/30,
-              //     child: Transform.scale(
-              //       scale: 0.5,
-              //       child: InkWell( 
-              //         // splashRadius: 50,
-              //         child: Container(
-              //           child: Image.asset(
-              //             "assets/images/writeimg.png",
-              //             fit: BoxFit.cover,
-              //           ),
-              //         ),
-              //         onTap: () {
-              //           print("이 버튼은 절대 누르지 마세요.");
-              //           Navigator.push(
-              //             context,
-              //             MaterialPageRoute(builder: (context) => const LetterScreen()),
-              //           );
-              //         },
-              //       ),
-              //     ),
-              //   ),
-              //   ],
-              // ),
-//               Icon(Icons.directions_bike),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   }
-
-//   Container createFruit(BuildContext context, double x, double y, int id) {
-//     return Container(
-//             child: Positioned(
-//               top: x,
-//               right: y,
-//               child: Container(
-//                 child:
-//                   IconButton( 
-//                     icon: Transform.scale(
-//                       scale: 3.5,
-//                       child: Image.asset(
-//                         "assets/images/apple.png",
-//                         fit: BoxFit.cover,
-//                       ),
-//                     ),
-//                     iconSize: 30,
-//                     onPressed: () {
-//                       print("사과를 누르지 마세요.");
-//                       FlutterDialog(context, id);
-//                     },
-//                   ),
-//               ),
-//             ),
-//           );
-//   }
-
-  // void FlutterDialog(BuildContext context, int id) {
-  //   double deviceWidth = MediaQuery.of(context).size.width;  // 화면의 가로 크기
-  //   double deviceHeight = MediaQuery.of(context).size.height; // 화면의 세로 크기
-  //   showDialog(
-  //       context: context,
-  //       //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
-  //       barrierDismissible: false,
-  //       builder: (BuildContext context) {
-  //         return AlertDialog(
-  //           // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
-  //           shape: RoundedRectangleBorder(
-  //               borderRadius: BorderRadius.circular(10.0)),
-  //           //Dialog Main Title
-  //           title: Column(
-  //             children: <Widget>[
-  //               new Text("편지의 제목"),
-  //             ],
-  //           ),
-  //           // title: Text("편지"),
-  //           //
-  //           content: Container(
-  //             alignment: Alignment.center,
-  //             child: SingleChildScrollView(
-  //               child: Column(
-  //                 crossAxisAlignment: CrossAxisAlignment.stretch,
-  //                 children: <Widget>[
-  //                   Text(
-  //                     '$id번째 사과를 눌렀더니 플러터를 사용해서 어플리케이션을 제작하는 영욱이가 긴 글을 그래도 겉보기에 괜찮아 보이도록 만드려는 노력 끝에 이런 의미 없는 문장들을 나열하기로 결정했어요. 이 문장은 여러분, 대단하게도 아무 의미가 없습니다. 그저 긴 글을 어느 정도의 수준으로 보여줄 지가 궁금했을 뿐입니다. 그런데 생각보다 쓸모가 없더라도 긴 글을 타이핑 하는 것은 쉬운 일이 아니군요. 저는 여태까지 많은 이들에게 편지를 써 왔는데요, 그 편지를 받는 이들은 같은 내용을 카카오톡을 통해 전달 받더라도 분명히 감사해야 합니다. 그렇게 쓰는 것도 분명 쉬운 일이 아니기 때문이죠. 저희 조는 수지 덕분에 꽤나 많은 정보를 갖고 시작했습니다. 수지는 엄청나게 많은 것을 알고 있습니다. 머릿속이 마치 도라에몽의 주머니 같달까요. 이럴 땐 그 결과를 부러워하기 보다는, 그가 그런 결과를 이뤄내기 위해 얼마나 많은 노력을 했을지에 대해 생각하며 스스로를 돌아봐야 합니다. 이 문장은 여러분, 대단하게도 아무 의미가 없습니다. 그저 긴 글을 어느 정도의 수준으로 보여줄 지가 궁금했을 뿐입니다. 그런데 생각보다 쓸모가 없더라도 긴 글을 타이핑 하는 것은 쉬운 일이 아니군요. 저는 여태까지 많은 이들에게 편지를 써 왔는데요, 그 편지를 받는 이들은 같은 내용을 카카오톡을 통해 전달 받더라도 분명히 감사해야 합니다. 그렇게 쓰는 것도 분명 쉬운 일이 아니기 때문이죠. 저희 조는 수지 덕분에 꽤나 많은 정보를 갖고 시작했습니다. 수지는 엄청나게 많은 것을 알고 있습니다. 머릿속이 마치 도라에몽의 주머니 같달까요. 이럴 땐 그 결과를 부러워하기 보다는, 그가 그런 결과를 이뤄내기 위해 얼마나 많은 노력을 했을지에 대해 생각하며 스스로를 돌아봐야 합니다.',
-  //                     style: TextStyle(fontSize: 16.0),
-  //                   ),
-  //                   // getLetterById.getContents(),
-  //                 ],
-  //               ),
-  //             ),
-  //           ),
-  //           actions: <Widget>[
-  //             Align(
-  //               alignment: Alignment.center,
-  //               child: TextButton(
-  //                 // alignment: Alignme
-  //                 child: new Text("잘 읽었어요"),
-  //                 onPressed: () {
-  //                   Navigator.pop(context);
-  //                 },
-  //               ),
-  //             ),
-  //           ],
-  //         );
-  //       });
-  // }
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         ),
-//         body: const Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               Text(
-//                 'name',
-//               ),
-//             ],
-//           )
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// Future<void> dbConnector() async {
-//   print("Connecting to mysql server ...");
-
-//   final settings = ConnectionSettings(
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'user',
-//     password: 'factory',
-//     db: 'factory_db',
-//   );
-
-//   final conn = await MySqlConnection.connect(settings);
-
-//   print("Connected");
-
-//   // Results? result;
-//   // int id = 1;
-
-//   // if (queryState == 'selectAll') {
-//   //   result = await conn.query("SELECT * FROM post");
-//   // } else if (queryState == 'select') {
-//   //   result = await conn.query(
-//   //       "SELECT * FROM post where id", [id]);
-//   // }
-
-//   // if (result != null) {
-//   //   for (var row in result) {
-//   //     print(row.fields);
-//   //   }
-//   // }
-
-//   Results? result = await crud.CrudService(conn: conn, tableName: "post").getDataList();
-//   // Results? result = await crud.CrudService(conn: conn, tableName: "post").findById(2);
-//   // Results? result = await crud.CrudService(conn: conn, tableName: "post").create(Post(id: 1 ,statement: "isitworkflutter", thanks: "happy", writer: "me"));
-//   // Results? result = await crud.CrudService(conn: conn, tableName: "post").update(Post(id: 1 ,statement: "changeyourstate!", thanks: "happy", writer: "me"));
-//   // Results? result = await crud.CrudService(conn: conn, tableName: "post").delete(6);
-
-//   print(result);
-
-//   await conn.close();
-// }
-
-// Future<http.Response> fetchPost() async {
-//   return http.get(Uri.parse("http://localhost:8081"));
-// }
-
-// // Future<http.Response> testHttpPost() async {
-// //   Post requestBody = Post.fromJson(json);
-// //   return http.post(Uri.parse("http://localhost:8081"), body: requestBody.toJson());
-// // }
